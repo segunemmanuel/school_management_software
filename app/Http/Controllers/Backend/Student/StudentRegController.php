4f0b6b9d5690f81b834b25cb967157035af3ce8c
segun <segunemmanuel46@gmail.com>
2021-10-16 00:25:29 -0700

    Update StudentRegController.php

--- app/Http/Controllers/Backend/Student/StudentRegController.php
+++ app/Http/Controllers/Backend/Student/StudentRegController.php
@@ -25,8 +25,8 @@ public function ViewStudentReg(){
     $data['years'] = StudentYear::all();
     $data['classes'] = StudentClass::all();
 
-    $data['year_id'] = StudentYear::orderBy('id','desc')->first()->id;
-    $data['class_id'] = StudentClass::orderBy('id','desc')->first()->id;
+    $data['year_id'] = StudentYear::orderBy('id','DESC')->first()->id;
+    $data['class_id'] = StudentClass::orderBy('id','DESC')->first()->id;
 
 $data['allData'] = AssignStudent::where('year_id',$data['year_id'])->where('class_id',$data['class_id'])->get();
 return view('backend.student.student_reg.student_view',$data);
