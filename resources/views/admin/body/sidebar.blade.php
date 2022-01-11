c6f0f0518853711d5818ad34494ffa39567cb9e0
segun <segunemmanuel46@gmail.com>
2022-01-11 00:21:19 -0800

    Grades and marks

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -117,10 +117,28 @@ $route = Route::current()->getName();
               <li><a href="{{ route('employee.salary.view')}}"><i class="ti-more"></i>Employee salary</a></li>
               <li><a href="{{ route('employee.leave.view')}}"><i class="ti-more"></i>Employee leave</a></li>
               <li><a href="{{ route('employee.attendance.view')}}"><i class="ti-more"></i>Employee attendance</a></li>
+              <li><a href="{{ route('employee.monthly.salary')}}"><i class="ti-more"></i>Employee monthly salary</a></li>
+
             </ul>
           </li>
 
 
+          <li class="treeview {{ ($prefix == '/marks')?'active':'' }}">
+            <a href="#">
+               <i data-feather="edit-2"></i> <span> Marks Management</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-right pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+          <li class="{{ ($route == 'marks.entry.add')?'active':'' }}"><a href="{{ route('marks.entry.add') }}"><i class="ti-more"></i>Marks Entry</a></li>
+        <li class="{{ ($route == 'marks.entry.edit')?'active':'' }}"><a href="{{ route('marks.entry.edit') }}"><i class="ti-more"></i>Marks Edit</a></li>
+
+         <li class="{{ ($route == 'marks.entry.grade')?'active':'' }}"><a href="{{ route('marks.entry.grade') }}"><i class="ti-more"></i>Marks Grade</a></li>
+
+
+            </ul>
+          </li>
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\Backend\Employee\EmployeeAttendanceController;
 use App\Http\Controllers\Backend\Employee\EmployeeLeaveController;
 use App\Http\Controllers\Backend\Employee\EmployeeRegController;
 use App\Http\Controllers\Backend\Employee\EmployeeSalaryController;
+use App\Http\Controllers\Backend\Employee\MonthlySalaryController;
+use App\Http\Controllers\Backend\Marks\GradeController;
+use App\Http\Controllers\Backend\Marks\MarksController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\Setup\AssignSubjectController;
 use App\Http\Controllers\Backend\Setup\ExamTypeController;
@@ -21,8 +24,8 @@ use App\Http\Controllers\Backend\Student\MonthlyFeeController;
 use App\Http\Controllers\Backend\Student\RegistrationFeeController;
 use App\Http\Controllers\Backend\Student\StudentRollController;
 use App\Http\Controllers\Backend\UserController;
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
 
 
 // StudentYearController
@@ -257,12 +260,47 @@ Route::get('attendance/employee/details/{date}', [EmployeeAttendanceController::
 
 
 
+// Employee Monthly Salary All Routes
+Route::get('monthly/salary/view', [MonthlySalaryController::class, 'MonthlySalaryView'])->name('employee.monthly.salary');
+
+Route::get('monthly/salary/get', [MonthlySalaryController::class, 'MonthlySalaryGet'])->name('employee.monthly.salary.get');
+
+Route::get('monthly/salary/payslip/{employee_id}', [MonthlySalaryController::class, 'MonthlySalaryPayslip'])->name('employee.monthly.salary.payslip');
 
 
 
 
 
+/// Marks Management Routes
+Route::prefix('marks')->group(function(){
 
+    Route::get('marks/entry/add', [MarksController::class, 'MarksAdd'])->name('marks.entry.add');
+
+    Route::post('marks/entry/store', [MarksController::class, 'MarksStore'])->name('marks.entry.store');
+
+    Route::get('marks/entry/edit', [MarksController::class, 'MarksEdit'])->name('marks.entry.edit');
+
+    Route::get('marks/getstudents/edit', [MarksController::class, 'MarksEditGetStudents'])->name('student.edit.getstudents');
+
+    Route::post('marks/entry/update', [MarksController::class, 'MarksUpdate'])->name('marks.entry.update');
+
+    // Marks Entry Grade
+
+    Route::get('marks/grade/view', [GradeController::class, 'MarksGradeView'])->name('marks.entry.grade');
+
+    Route::get('marks/grade/add', [GradeController::class, 'MarksGradeAdd'])->name('marks.grade.add');
+
+    Route::post('marks/grade/store', [GradeController::class, 'MarksGradeStore'])->name('store.marks.grade');
+
+    Route::get('marks/grade/edit/{id}', [GradeController::class, 'MarksGradeEdit'])->name('marks.grade.edit');
+
+    Route::post('marks/grade/update/{id}', [GradeController::class, 'MarksGradeUpdate'])->name('update.marks.grade');
+
+    });
+
+    Route::get('marks/getsubject', [DefaultController::class, 'GetSubject'])->name('marks.getsubject');
+
+    Route::get('student/marks/getstudents', [DefaultController::class, 'GetStudents'])->name('student.marks.getstudents');
 
 
 
